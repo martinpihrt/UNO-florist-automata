bb5b686009d82c0c2fef69164657ec0fc6356654
Martin Pihrt <martinpihrt@gmail.com>
2018-10-14 20:45:23 +0200

    LCD foto

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 UNO florist automata Readme
 ====
 
-UNO florist automata - Arduino UNO based floristics automata.
+UNO florist automata - Zavlažovač květin založený na platformě Arduino a desce Arduino UNO (tedy procesoru ATMEGA328).
+
+Regulace dle vlhkosti:
+a) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a vlhkost < 40 bude výsledná doba trvání závlahy 50% nastavené doby v menu
+b) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a vlhkost >= 40 a < 60 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+c) pokud je danný den a je čas programu 1 nebo je čas programu 2 a vlhkost >= 60 a < 80 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+d) pokud je danný den a je čas programu 1 a vlhkost >= 60 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu             
+
+Regulece dle teploty:
+a) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a teplota > 40 bude výsledná doba trvání závlahy 50% nastavené doby v menu
+b) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a teplota >= 30 a < 40 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu               
+c) pokud je danný den a je čas programu 1 nebo je čas programu 2 a teplota >= 20 a < 30 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+d) pokud je danný den a je čas programu 1 a teplota >= 5 a < 20 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+
+Cyklování výstupu (relé, čerpadlo...):
+v menu lze nastavit po jaké době běhu programu (1, 2, 3) se má výstup čerpadla vypnout a jako dobu má čekat vypnutý. Tato funkce umožňuje cyklování výstupu pro lepší vsakování vody do půdy (nedojde k přetékání květináče, ale k postupnému vasku vody)
 
 Ve výstavbě...
 
-## More information visit
-Martin Pihrt - pihrt.com
+## Pro více informací navštivte web autora Martin Pihrt - www.pihrt.com
 
--- sw/main_1.00/main_1.00.ino
+++ sw/main_1.00/main_1.00.ino
@@ -8,6 +8,21 @@ lze nastavit pouzivani cidla vlhkosti nebo teploty a dle toho regulovat
 lze nastavit datum a cas
 lze nastavovat a vypisovat po seriove lince
 
+Regulace dle vlhkosti:
+a) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a vlhkost < 40 bude výsledná doba trvání závlahy 50% nastavené doby v menu
+b) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a vlhkost >= 40 a < 60 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+c) pokud je danný den a je čas programu 1 nebo je čas programu 2 a vlhkost >= 60 a < 80 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+d) pokud je danný den a je čas programu 1 a vlhkost >= 60 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu             
+
+Regulece dle teploty:
+a) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a teplota > 40 bude výsledná doba trvání závlahy 50% nastavené doby v menu
+b) pokud je danný den a je čas programu 1 nebo je čas programu 2 nebo je čas programu 3 a teplota >= 30 a < 40 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu               
+c) pokud je danný den a je čas programu 1 nebo je čas programu 2 a teplota >= 20 a < 30 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+d) pokud je danný den a je čas programu 1 a teplota >= 5 a < 20 bude výsledná doba trvání závlahy stejná jako nastavená doba v menu
+
+Cyklování výstupu (relé, čerpadlo...):
+v menu lze nastavit po jaké době běhu programu (1, 2, 3) se má výstup čerpadla vypnout a jako dobu má čekat vypnutý. Tato funkce umožňuje cyklování výstupu pro lepší vsakování vody do půdy (nedojde k přetékání květináče, ale k postupnému vasku vody)
+
 *** MENU *************
 ? Napoveda
 A Spustit program 1
@@ -1095,7 +1110,7 @@ void vypisuj_info_lcd(){ // informace na lcd
            lcd.setCursor(0,0);
            tisk_casu();
            lcd.setCursor(0,1);
-           if(frq==0) {lcd.print(msg_err_cidla);lcd.print(msg_otaznik);}
+           if(frq==0) {lcd.print(msg_err_cidla);lcd.print(msg_otaznik);lcd.print(prazdno);}
            else {lcd.print(vlhkost);lcd.print(msg_procenta);lcd.print(mezera);}
            if(frq<1000 && frq>0)  {lcd.print(frq); lcd.print(msg_Hz);}
            else{lcd.print(frq/1000); lcd.print(msg_kHz);}
